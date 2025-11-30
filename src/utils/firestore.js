356bd99f4693d94dbb8ba20f5b462340dc664a18
sarthak-smartq <sarthak.gupta@thesmartq.com>
2025-11-30 23:52:34 +0530

    ADDING firebase cred

--- src/utils/firestore.js
+++ src/utils/firestore.js
@@ -1,22 +1,31 @@
 // firestore.js
 import admin from "firebase-admin";
+import dotenv from 'dotenv'
 
 let app;
+
+dotenv.config()
 
 // Prevent initializing multiple times in dev mode (nodemon)
 if (!admin.apps.length) {
   app = admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(
+      {
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      }
+    ),
   });
 } else {
   app = admin.app();
 }
 
-export const firestore = admin.firestore();
+export const firebase = admin.firestore();
 
 // Optional: set Firestore settings for better performance
-firestore.settings({
+firebase.settings({
   ignoreUndefinedProperties: true,
 });
 
-export default firestore;
+export default firebase;
